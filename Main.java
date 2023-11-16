ef71524f015da988bfd0a09c656ffb6ac6df5b49
SearCoding <148085713+SearCoding@users.noreply.github.com>
2023-11-16 14:08:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Prj 1 - Hello World - Fall 2023 ");
+    System.out.println("Author: Nasir Deshields");
+    
+    System.out.println("\nHello world!");
+
+    System.out.println("Project is finished Running!");
+  }
+}
